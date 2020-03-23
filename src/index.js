9b0b49afd98869cf66636bf93dfa69c4e959803e
Felix Anducho <felix.anducho74@gmail.com>
2020-03-22 19:04:37 -0600

    Add files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+const Hello = props =>{
+  const bornYear = () =>{
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old.
+      </p>
+    <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+*/
+
+const App = (props) =>{
+  const [counter, setCounter ] = useState(0);
+
+  
+
+  const increaseByOne = () => setCounter(counter + 1);
+  
+  const decreaseByOne = () => setCounter(counter - 1)
+
+  const resetButton = () => setCounter(0);
+
+  const Display = ({ counter }) => <div>{ counter}</div>
+
+  const Button = ({handleClick, text}) =>{
+    return (
+      <button onClick={handleClick}>
+        {text}
+      </button>
+    )
+  }
+
+
+    return (
+    <div>
+      <Display counter={counter}/>
+      <Button 
+      handleClick={increaseByOne} 
+      text={'Plus'}
+      />
+
+      <Button 
+      handleClick={decreaseByOne} 
+      text={'Minus'}
+      />
+
+      <Button 
+      handleClick={resetButton} 
+      text={'Reset'}
+      />
+    </div>
+    )
+}
+
+let counter = 1;
+
+  ReactDOM.render(
+    <App counter={counter}/>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
